586134645e1a679494102e64c1a66c9914abfdb4
Bj'z <zbjarchi@gmail.com>
2023-03-26 14:13:22 +0200

    :lipstick: add Main Title
    <h1>inner the <Router>

---
+++ learn-redux/client/components/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Main = () => {
+  return (
+    <div>
+      <h1>
+        <Link to="/">InstaDux</Link>
+      </h1>
+    </div>
+  );
+};
+
+export default Main;
