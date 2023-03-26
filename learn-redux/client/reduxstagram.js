d6cccb4491c51979214fff1d18c6b9451f2934c5
Bj'z <zbjarchi@gmail.com>
2023-03-26 15:15:33 +0200

    :bug: Fix: router
    IndexRoute as a parent

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -2,7 +2,24 @@ import React from "react";
 import { render } from "react-dom";
 
 //import css
-
 import css from "./styles/style.styl";
 
-render(<p>heyo</p>, document.getElementById("root"));
+//Import Components
+import Main from "./components/Main";
+import PhotoGrid from "./components/PhotoGrid";
+import Single from "./components/Single";
+
+// import react router deps
+
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+);
+
+render(router, document.getElementById("root"));
